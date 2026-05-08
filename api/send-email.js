ec877d8d25bfa47bb4d40f699fefa2a79482e2f3
clubenacionalpremios-glitch <clubenacionalpremios@gmail.com>
2026-05-08 17:40:24 -0300

    Create send-email.js

---
+++ api/send-email.js
@@ -0,0 +1,80 @@
+export default async function handler(req, res) {
+
+  if (req.method !== "POST") {
+    return res.status(405).json({
+      erro: "Método não permitido"
+    });
+  }
+
+  try {
+
+    const { email, nome, pin } = req.body;
+
+    const resposta = await fetch("https://api.resend.com/emails", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer re_BcUh84nC_44miQ7tgSnEc8WXwUSosabE8"
+      },
+      body: JSON.stringify({
+        from: "Clube Nacional <cadastro@clubenacionaldepremios.com>",
+        to: email,
+        subject: "Confirme seu cadastro - Clube Nacional de Prêmios",
+
+        html: `
+          <div style="font-family:Arial;padding:20px;line-height:1.6">
+            <h2>Cadastro realizado com sucesso 🎉</h2>
+
+            <p>Olá, ${nome}</p>
+
+            <p>
+              Seu cadastro foi realizado com sucesso no
+              <strong>Clube Nacional de Prêmios</strong>.
+            </p>
+
+            <p>
+              Seu número da sorte é:
+            </p>
+
+            <div style="
+              background:#111;
+              color:white;
+              padding:15px;
+              border-radius:10px;
+              display:inline-block;
+              font-size:22px;
+              font-weight:bold;
+            ">
+              ${pin}
+            </div>
+
+            <p style="margin-top:25px">
+              Guarde este número. Ele será utilizado nos sorteios.
+            </p>
+
+            <p>
+              Boa sorte 🍀
+            </p>
+          </div>
+        `
+      })
+    });
+
+    const data = await resposta.json();
+
+    if (!resposta.ok) {
+      return res.status(500).json(data);
+    }
+
+    return res.status(200).json({
+      sucesso: true
+    });
+
+  } catch (erro) {
+
+    return res.status(500).json({
+      erro: erro.message
+    });
+
+  }
+}
